9fde0e67a0054823dfe0b95c9e6e13dd3272ec31
Aery <aeryowo@gmail.com>
2019-01-11 12:56:43 +0100

    Began on ConfigFile n format stuffs ^w^

---
+++ src/ConfigFile.ts
@@ -0,0 +1,38 @@
+import {
+    readFile,
+    writeFile
+} from "fs";
+import * as path from "path";
+
+export class ConfigFile {
+
+    path: string;
+    format: () => any;
+    default_content: {};
+
+    private default_options: any;
+
+    constructor(path: string, format: () => any) {
+        this.path = path;   
+        this.format = format;
+    }
+
+    def(default_content: {}, options?: any): ConfigFile {
+        this.default_content = default_content;
+        this.default_options = options;
+        return this;
+    }
+
+    read(write_if_defaulted?: boolean): Promise<any> {
+        return new Promise((resolve, reject) => {
+            readFile(this.path, (error, data) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    
+                }
+            });
+        });
+    }
+
+}
---
+++ src/formats.ts
@@ -0,0 +1,6 @@
+export const formats: {} = {};
+export const file_extention_formats: {} = {};
+
+export function registerFormat(name: string, file_extentions: string[]): void {
+
+}
